f6aecd1fcc97bc780351d7b8bd84bb4910ca9727
andoyeon <56617914+andoyeon@users.noreply.github.com>
2019-12-04 17:56:00 +0900

    12/04

--- scratch09/ex08.py
+++ scratch09/ex08.py
@@ -5,25 +5,33 @@ lec08_database 패키지의 내용들을 참고해서,
 csv 모듈을 사용해서, csv 파일(emp.csv)로 저장
 """
 import cx_Oracle
-import lec08_database.oracle_config as cfg
 import csv
 
-emp = []
-with cx_Oracle.connect(cfg.user, cfg.pwd, cfg.dsn) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute('select * from emp')
-        for row in cursor:
-            emp.append(list(row))
 
-print(emp)
+if __name__ == '__main__':
+    # DSN: Data Source Name(접속할 오라클 서버(호스트) 정보)
+    dsn = cx_Oracle.makedsn('localhost', 1521, 'orcl')
+    # 오라클 데이터베이스 서버에 접속(connection)
+    with cx_Oracle.connect('scott', 'tiger', dsn) as connection:
+        # cursor 객체(SQL문을 데이터베이스 서버에서 실행) 생성
+        with connection.cursor() as cursor:
+            # SQL 문장 작성
+            sql_select_emp = 'select * from emp'
+            # SQL 문장을 DB 서버에서 실행
+            cursor.execute(sql_select_emp)
+            # SQL 실행 결과 처리 -> 리스트에 저장
+            emp = [row for row in cursor]
+            print(emp[0])
+            print(len(emp))
 
+    # 리스트 emp의 내용을 파일에 csv 형식으로 저장
+    file_path = 'emp.csv'
+    # 파일을 쓰기 모드로 열기
+    with open(file_path, mode='w', encoding='UTF-8', newline='') as f:
+        writer = csv.writer(f)  # csv writer 객체를 생성
+        for item in emp:    # 리스트의 각 아이템마다 반복
+            writer.writerow(item)   # 아이템을 csv 파일에 한 줄씩 쓰기
 
-
-
-with open('emp.csv', mode='w', encoding='utf-8', newline='') as f:
-    writer = csv.writer(f, delimiter=',')
-    for row in emp:
-        writer.writerow(row)
 
 
 
--- scratch09/ex09.py
+++ scratch09/ex09.py
@@ -12,43 +12,73 @@ emp.csv 파일을 읽어서, DataFrame을 생성
 - DataFrame을 csv 형식으로 파일에 write하는 함수를 찾아서 데이터 프레임을 파일로 저장
 """
 import pandas as pd
-import numpy as np
 
-mpg = pd.read_csv('emp.csv', encoding='utf-8', header=None)
-mpg.columns = ['empno', 'ename', 'job', 'mgr', 'hiredate', 'sal', 'comm', 'deptno']
-print(mpg)
+if __name__ == '__main__':
+    file_path = 'emp.csv'
+    # pandas.read_csv() 함수는 csv 파일의 첫번째 줄을 헤더(컬럼 이름)으로 취급
+    # csv 파일의 첫번째 줄이 헤더(컬럼 이름)가 아니고 실제 레코드인 경우에는
+    # read_csv() 함수를 호출할 때 header=None 파라미터를 추가해야 함.
+    emp = pd.read_csv(file_path, header=None)
+    print('shape:', emp.shape)
+    print(emp.head())
 
-# 급여(sal)가 2000 이상인 직원들의 모든 정보를 출력
-print(mpg[mpg['sal'] >= 2000])
+    # DataFrame 컬럼 이름 설정
+    emp.columns =  ['empno', 'ename', 'job', 'mgr', 'hiredate', 'sal', 'comm', 'deptno']
+    print(emp.iloc[0:2])
 
-# 부서 번호(deptno)가 10인 직원들의 모든 정보를 출력
-print(mpg[mpg['deptno'] == 10])
+    print('\n급여(sal)가 2000 이상인 직원들의 모든 정보')
+    print(emp[emp['sal'] >= 2000])  # DataFrame[조건식]
 
-# 급여가 전체 직원의 급여의 평균보다 많은 직원의 사번, 이름, 급여를 출력
-sal_mean = mpg['sal'].mean()
-print(f'sal 평균: {sal_mean}')
-print(mpg[mpg['sal'] > sal_mean][['empno', 'ename', 'sal']])
+    print('\n부서 번호(deptno)가 10인 직원들의 모든 정보')
+    print(emp[emp['deptno'] == 10])
 
-# 30번 부서에서 일하는, 직책이 SALESMAN인 직원들의 사번, 이름, 급여, 부서번호를 출력
-cond1 = (mpg['deptno'] == 30)
-cond2 = (mpg['job'] == 'SALESMAN')
-print(mpg[cond1 & cond2][['empno', 'ename', 'sal', 'deptno']])
+    print('\n급여가 전체 직원의 급여의 평균보다 많은 직원의 사번, 이름, 급여')
+    # select empno, ename, sal from emp where sal > (select avg(sal) from emp);
+    subset = emp[emp['sal'] > emp['sal'].mean()]
+    print(subset[['empno', 'ename', 'sal']])
 
-# 20번, 30번 부서에 근무하는 직원들 중 급여가 2000을 초과하는
-#   직원들의 사번, 이름, 급여, 부서번호를 출력
-cond1 = (mpg['deptno'] == 20) | (mpg['deptno'] == 30)
-print(cond1)
-cond2 = (mpg['sal'] > 2000)
-print(mpg[cond1 & cond2][['empno', 'ename', 'sal', 'deptno']])
+    print('\n30번 부서에서 일하는, 직책이 SALESMAN인 직원들의 사번, 이름, 급여, 부서번호')
+    # select empno, ename, job, sal, deptno
+    # from emp
+    # where deptno = 30 and job = 'SALESMAN';
+    c1 = emp['deptno'] == 30   # 30번 부서에서 일하는
+    c2 = emp['job'] == 'SALESMAN'  # 직책이 SALESMAN인
+    subset = emp[c1 & c2]
+    print(subset[['empno', 'ename', 'sal', 'deptno']])
 
-# 수당이 없는 직원들 중에서, 매니저가 있고, 직책이 'MANAGER' 또는 'CLERK'인
-#   직원들의 모든 정보를 검색
-cond1 = mpg['comm'].isnull()
-cond2 = (mpg['job'] == 'MANAGER') | (mpg['job'] == 'CLERK')
-print(mpg[cond1 & cond2])
+    print('\n20번, 30번 부서에 근무하는 직원들 중 급여가 2000을 초과하는 직원들')
+    # select * from emp
+    # where (deptno = 20 or deptno = 30) and sal > 2000;
+    # select * from emp
+    # where deptno in (20, 30) and sal > 2000;
 
-# 사원 이름에 'E'가 포함된 직원들의 이름만 출력(str.contains() 이용)
-print(mpg[mpg['ename'].str.contains('E')])
+    # c1 = emp['deptno'] == 20
+    # c2 = emp['deptno'] == 30
+    # c3 = emp['sal'] > 2000
+    # subset = emp[(c1 | c2) & c3]
+    c1 = emp['deptno'].isin([20, 30])
+    c2 = emp['sal'] > 2000
+    subset = emp[c1 & c2]
+    print(subset[['empno', 'ename', 'sal', 'deptno']])
 
-# DataFrame을 csv 형식으로 파일에 write하는 함수를 찾아서 데이터 프레임을 파일로 저장
-mpg.to_csv('mpg.csv', mode='w', encoding='utf-8')
+    print('\n수당이 없는 직원들 중에서, 매니저가 있고, 직책이 MANAGER 또는 CLERK인 직원들')
+    # select * from emp
+    # where comm is null and mgr is not null and job in ('MANAGER', 'CLERK')
+
+    c1 = emp['comm'].isna() # 수당이 없는 직원. emp['comm'] == 'NaN' 이라고 하면 안됨.
+    c2 = ~emp['mgr'].isna() # mgr 컬럼의 값이 NaN(null)이 아닌 경우. ~ not 연산자
+    # c3 = (emp['job'] == 'MANAGER') | (emp['job'] == 'CLERK')
+    c3 = emp['job'].isin(['MANAGER', 'CLERK'])  # job이 manager 또는 clerk
+    subset = emp[c1 & c2 & c3]
+    print(subset)
+
+    print('\n사원 이름에 E가 포함된 직원들')
+    # select ename from emp where ename like '%E%';
+    subset = emp[emp['ename'].str.contains('E')]
+    print(subset['ename'])  # Series
+    print(subset[['ename']])  # DataFrame
+
+    # DataFrame.to_csv(file_path): 데이터 프레임을 csv 파일로 저장
+    # to_csv() 함수는 행 이름(row index)를 파일에 씀.
+    # row index를 파일에 쓰지 않으려면, 함수를 호출할 때 index=False 파라미터 추가.
+    emp.to_csv('emp2.csv', index=False)
